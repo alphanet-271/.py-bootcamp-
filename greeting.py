f9dd9052a3a5464191bd9ad0e7c700cbe3af6292
Abdulmuheez Ibraheem <125488211+alphanet-271@users.noreply.github.com>
2025-05-06 00:29:26 +0100

    Add files via upload

---
+++ greeting.py
@@ -0,0 +1,6 @@
+#A program that takes input of name and age and display to user
+
+userName = input("Enter your name: ")
+userAge = input("Enter your age: ")
+
+print(f'Good day {userName}!\n You are {userAge} years old')
---
+++ tempConverter.py
@@ -0,0 +1,7 @@
+""" A program that performs temperature conversion
+Celsius to Fahrenheit = (C x 1.8) + 32
+"""
+print("This program convert temperature in celsius to fahrenheit")
+celsisus = int(input("Enter the value of Celsius to be converted: "))
+fahrenheit = (celsisus * 1.8) + 32
+print(f'Temp in Fahrenheit: {fahrenheit}F')
